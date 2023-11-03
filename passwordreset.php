83cf43b05484f051dd8a26b33c722fe97b6c0b70
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-03 15:28:55 +0100

    Update and rename passreset.php to passwordreset.php

--- passwordreset.php
+++ passwordreset.php
@@ -5,11 +5,11 @@ $token=$_GET['token'];
 $user=$_GET['user'];
 $pass=mt_rand();
 
-$query = "Update register set password='$pass' where username='$user'";
+$query = "Update user set password='$pass' where username='$user'";
 if (mysqli_query($db, $query)==1)
 {
 
-    echo 'Your new new password is   '.$pass;
+    echo 'Your new password is   '.$pass;
 }
 else{
 	echo "Error changing password";

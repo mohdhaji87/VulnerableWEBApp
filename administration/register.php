17668a10a816f21defcd850f0df46cf4ffabe7a6
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-04 23:32:42 +0100

    Update register.php

--- administration/register.php
+++ administration/register.php
@@ -26,27 +26,20 @@ if (isset($register)) {
             <div style="width:600px; margin:0px auto">
 
             <form class="" action="" method="post">
-                <div class="form-group pt-3">
-                  <label for="name">Your name</label>
-                  <input type="text" name="name"  class="form-control">
-                </div>
                 <div class="form-group">
                   <label for="username">Your username</label>
                   <input type="text" name="username"  class="form-control">
                 </div>
                 <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" name="password" class="form-control">
+                  <input type="hidden" name="role_id" value="2" class="form-control">
+                </div>
+                <div class="form-group">
                   <label for="email">Email address</label>
                   <input type="email" name="email"  class="form-control">
                 </div>
-                <div class="form-group">
-                  <label for="mobile">Mobile Number</label>
-                  <input type="text" name="mobile"  class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input type="password" name="password" class="form-control">
-                  <input type="hidden" name="roleid" value="3" class="form-control">
-                </div>
+
                 <div class="form-group">
                   <button type="submit" name="register" class="btn btn-success">Register</button>
                 </div>

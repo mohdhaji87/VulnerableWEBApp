868a5b810adb0da743838bf9ae204431b426cfcf
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-04 21:21:01 +0100

    Create project.sql

---
+++ project.sql
@@ -0,0 +1,31 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `role` (`role_id`, `role_type`) VALUES
+(1, 'Admin'),
+(2, 'User');
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `avatar_id` varchar(255) DEFAULT NULL,
+  `role_id` tinyint(4) DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `avatar_id`, `role_id`, `isActive`) VALUES
+(1, 'admin', 'admin', 'admin@admin.admin', NULL, 1, 0),
+COMMIT;

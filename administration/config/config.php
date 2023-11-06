f7edb832b23be00696a3a227286e98a8fedd0b6b
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-06 12:00:29 +0100

    Update config.php

--- administration/config/config.php
+++ administration/config/config.php
@@ -3,5 +3,5 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'project');
-define('DB_USER', 'admin');
-define('DB_PASS', 'adminpass');
+define('DB_USER', 'test');
+define('DB_PASS', 'test');

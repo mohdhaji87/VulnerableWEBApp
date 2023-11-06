db7a4e18c40fd57bde4572171db98e7d2558ae7d
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-06 19:21:26 +0100

    Update project.sql

--- project.sql
+++ project.sql
@@ -17,7 +17,7 @@ INSERT INTO `role` (`role_id`, `role_type`) VALUES
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `email` varchar(255) DEFAULT NULL,
-  `isAdmin` tinyint(4) DEFAULT '0',
+  `isAdmin` tinyint(4) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `admin` (`email`, `isAdmin`) VALUES

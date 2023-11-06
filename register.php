c1335fc5410f416c0492b794e1ff1ea27d691618
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-06 11:57:47 +0100

    Update register.php

--- register.php
+++ register.php
@@ -13,7 +13,7 @@ include("config.php");
 $a=$_POST['username'];
 $b=sha1($_POST['password']);
 $c=$_POST['email'];
-$query = "insert into user (username, password, email) values ('$a', '$b','$c')";
+$query = "insert into user (username, password, email) values ('$a', '$b', '$c')";
 
 echo "" . '<br />';
 

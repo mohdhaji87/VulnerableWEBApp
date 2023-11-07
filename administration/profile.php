aca9b692b1424f43443d042913c174e984c20859
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-07 23:56:16 +0100

    Update profile.php

--- administration/profile.php
+++ administration/profile.php
@@ -107,9 +107,6 @@ if (isset($updateUser)) {
                 
                 <input type="hidden" name="role_id" value="<?php echo $getUinfo->role_id; ?>">
                 
-                <?php if (!$avatar_id){ ?>
-                  <input type="hidden" name="avatar_id" value="<?php echo $getUinfo->avatar_id; ?>">
-                   <?php } ?>
                    
           <?php if ( Session::get("role_id") == '1' && $users->CheckAdminUser(Session::get("email")) ) {?>
               <div class="form-group">

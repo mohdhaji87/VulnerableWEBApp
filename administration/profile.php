5b8df731b2bfd4785f158976ea7af3e090130dfb
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-08 00:29:24 +0100

    Update profile.php

--- administration/profile.php
+++ administration/profile.php
@@ -8,7 +8,7 @@ Session::CheckSession();
 
 if (isset(($_GET['user_id']))) {
   ## $user_id = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['user_id']);
-  if ((int)$_GET['user_id'){ 
+  if ((int)$_GET['user_id']){ 
    $user_id = (int)$_GET['user_id'];
    }
 }
